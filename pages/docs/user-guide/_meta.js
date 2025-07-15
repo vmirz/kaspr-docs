394d3a86a561e29bb88581303dd71394f33af251
Viktor Mirzoyan <44904186+vmirz@users.noreply.github.com>
2025-07-15 09:20:51 -0400

    agents user guide

--- pages/docs/user-guide/_meta.js
+++ pages/docs/user-guide/_meta.js
@@ -3,5 +3,6 @@ export default {
   //processors: 'Processors',
   "kafka-basics": "Kafka - Basics you need to know",
   agents: "Agents - Distributed stream processors",
+  patterns: "Agent Patterns - Common Use Cases",
   scheduler: "Scheduler - Time-Controlled Event Handling"
 }
